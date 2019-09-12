29b3af34e8a9a77bfe8d2a69a4bdca1c14fdf19d
arash <arashghelman77@gmail.com>
2019-09-12 18:46:59 +0430

    Added controls to PersonForm. Defined tooltips for each textbox. Defiend buttons Add and Refresh.

--- View/PersonForm.Designer.cs
+++ View/PersonForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lblFirstName = new System.Windows.Forms.Label();
             this.lblLastName = new System.Windows.Forms.Label();
             this.lblBirthYear = new System.Windows.Forms.Label();
@@ -40,12 +41,19 @@
             this.txtbxPhoneNumber = new System.Windows.Forms.TextBox();
             this.txtbxEmailAddress = new System.Windows.Forms.TextBox();
             this.cmbbxSex = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.dgvPerson = new System.Windows.Forms.DataGridView();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerson)).BeginInit();
             this.SuspendLayout();
             // 
             // lblFirstName
             // 
             this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Location = new System.Drawing.Point(75, 103);
+            this.lblFirstName.Location = new System.Drawing.Point(37, 46);
             this.lblFirstName.Name = "lblFirstName";
             this.lblFirstName.Size = new System.Drawing.Size(63, 13);
             this.lblFirstName.TabIndex = 0;
@@ -54,7 +62,7 @@
             // lblLastName
             // 
             this.lblLastName.AutoSize = true;
-            this.lblLastName.Location = new System.Drawing.Point(75, 129);
+            this.lblLastName.Location = new System.Drawing.Point(37, 72);
             this.lblLastName.Name = "lblLastName";
             this.lblLastName.Size = new System.Drawing.Size(64, 13);
             this.lblLastName.TabIndex = 1;
@@ -63,7 +71,7 @@
             // lblBirthYear
             // 
             this.lblBirthYear.AutoSize = true;
-            this.lblBirthYear.Location = new System.Drawing.Point(75, 159);
+            this.lblBirthYear.Location = new System.Drawing.Point(37, 102);
             this.lblBirthYear.Name = "lblBirthYear";
             this.lblBirthYear.Size = new System.Drawing.Size(56, 13);
             this.lblBirthYear.TabIndex = 2;
@@ -72,7 +80,7 @@
             // lblSex
             // 
             this.lblSex.AutoSize = true;
-            this.lblSex.Location = new System.Drawing.Point(75, 189);
+            this.lblSex.Location = new System.Drawing.Point(37, 132);
             this.lblSex.Name = "lblSex";
             this.lblSex.Size = new System.Drawing.Size(31, 13);
             this.lblSex.TabIndex = 3;
@@ -81,7 +89,7 @@
             // lblPhoneNumber
             // 
             this.lblPhoneNumber.AutoSize = true;
-            this.lblPhoneNumber.Location = new System.Drawing.Point(75, 214);
+            this.lblPhoneNumber.Location = new System.Drawing.Point(37, 157);
             this.lblPhoneNumber.Name = "lblPhoneNumber";
             this.lblPhoneNumber.Size = new System.Drawing.Size(81, 13);
             this.lblPhoneNumber.TabIndex = 4;
@@ -90,43 +98,43 @@
             // lblEmailAddress
             // 
             this.lblEmailAddress.AutoSize = true;
-            this.lblEmailAddress.Location = new System.Drawing.Point(75, 246);
+            this.lblEmailAddress.Location = new System.Drawing.Point(37, 189);
             this.lblEmailAddress.Name = "lblEmailAddress";
-            this.lblEmailAddress.Size = new System.Drawing.Size(73, 13);
+            this.lblEmailAddress.Size = new System.Drawing.Size(76, 13);
             this.lblEmailAddress.TabIndex = 5;
-            this.lblEmailAddress.Text = "Email Address";
+            this.lblEmailAddress.Text = "Email Address:";
             // 
             // txtbxFirstName
             // 
-            this.txtbxFirstName.Location = new System.Drawing.Point(153, 100);
+            this.txtbxFirstName.Location = new System.Drawing.Point(115, 43);
             this.txtbxFirstName.Name = "txtbxFirstName";
             this.txtbxFirstName.Size = new System.Drawing.Size(100, 20);
             this.txtbxFirstName.TabIndex = 6;
             // 
             // txtbxLastName
             // 
-            this.txtbxLastName.Location = new System.Drawing.Point(153, 126);
+            this.txtbxLastName.Location = new System.Drawing.Point(115, 69);
             this.txtbxLastName.Name = "txtbxLastName";
             this.txtbxLastName.Size = new System.Drawing.Size(100, 20);
             this.txtbxLastName.TabIndex = 7;
             // 
             // txtbxBirthYear
             // 
-            this.txtbxBirthYear.Location = new System.Drawing.Point(153, 152);
+            this.txtbxBirthYear.Location = new System.Drawing.Point(115, 95);
             this.txtbxBirthYear.Name = "txtbxBirthYear";
             this.txtbxBirthYear.Size = new System.Drawing.Size(100, 20);
             this.txtbxBirthYear.TabIndex = 8;
             // 
             // txtbxPhoneNumber
             // 
-            this.txtbxPhoneNumber.Location = new System.Drawing.Point(172, 211);
+            this.txtbxPhoneNumber.Location = new System.Drawing.Point(134, 154);
             this.txtbxPhoneNumber.Name = "txtbxPhoneNumber";
             this.txtbxPhoneNumber.Size = new System.Drawing.Size(100, 20);
             this.txtbxPhoneNumber.TabIndex = 9;
             // 
             // txtbxEmailAddress
             // 
-            this.txtbxEmailAddress.Location = new System.Drawing.Point(172, 243);
+            this.txtbxEmailAddress.Location = new System.Drawing.Point(134, 186);
             this.txtbxEmailAddress.Name = "txtbxEmailAddress";
             this.txtbxEmailAddress.Size = new System.Drawing.Size(100, 20);
             this.txtbxEmailAddress.TabIndex = 10;
@@ -134,16 +142,67 @@
             // cmbbxSex
             // 
             this.cmbbxSex.FormattingEnabled = true;
-            this.cmbbxSex.Location = new System.Drawing.Point(151, 181);
+            this.cmbbxSex.Location = new System.Drawing.Point(113, 124);
             this.cmbbxSex.Name = "cmbbxSex";
             this.cmbbxSex.Size = new System.Drawing.Size(121, 21);
             this.cmbbxSex.TabIndex = 11;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(415, 93);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(415, 124);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 13;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(415, 155);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(415, 184);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 15;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // dgvPerson
+            // 
+            this.dgvPerson.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPerson.Location = new System.Drawing.Point(85, 262);
+            this.dgvPerson.Name = "dgvPerson";
+            this.dgvPerson.Size = new System.Drawing.Size(240, 150);
+            this.dgvPerson.TabIndex = 16;
             // 
             // PersonForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(917, 490);
+            this.Controls.Add(this.dgvPerson);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.cmbbxSex);
             this.Controls.Add(this.txtbxEmailAddress);
             this.Controls.Add(this.txtbxPhoneNumber);
@@ -159,6 +218,7 @@
             this.Name = "PersonForm";
             this.Text = "PersonForm";
             this.Load += new System.EventHandler(this.PersonForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerson)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -178,5 +238,11 @@
         private System.Windows.Forms.TextBox txtbxPhoneNumber;
         private System.Windows.Forms.TextBox txtbxEmailAddress;
         private System.Windows.Forms.ComboBox cmbbxSex;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridView dgvPerson;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
--- View/PersonForm.cs
+++ View/PersonForm.cs
@@ -12,14 +12,58 @@ namespace View
 {
     public partial class PersonForm : Form
     {
+        #region [- ctor -]
         public PersonForm()
         {
             InitializeComponent();
+            Ref_PersonViewModel = new ViewModel.PersonViewModel();
         }
+        #endregion
 
+        #region [- PersonForm_Load -]
         private void PersonForm_Load(object sender, EventArgs e)
         {
             cmbbxSex.Items.AddRange(new string[] { "Male", "Female" });
+            AssignToolTip();
         }
+        #endregion
+
+        #region [- props -]
+        public ViewModel.PersonViewModel Ref_PersonViewModel { get; set; } 
+        #endregion
+
+        #region [- AssignToolTip() -]
+        public void AssignToolTip()
+        {
+            toolTip1.SetToolTip(txtbxFirstName, "Enter your first name \n You can only enter characters");
+            toolTip1.SetToolTip(txtbxLastName, "Enter your last name \n You can only enter characters");
+            toolTip1.SetToolTip(txtbxBirthYear, "Enter the year you were born \n You can only enter numbers");
+            toolTip1.SetToolTip(cmbbxSex, "Select your gender");
+            toolTip1.SetToolTip(txtbxPhoneNumber, "Enter your phone number \n You can only enter numbers");
+            toolTip1.SetToolTip(txtbxEmailAddress, "Enter your email address");
+        }
+        #endregion
+
+        #region [- BtnAdd_Click -]
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            Ref_PersonViewModel.Add
+                (
+                txtbxFirstName.Text,
+                txtbxLastName.Text,
+                Convert.ToInt32(txtbxBirthYear.Text),
+                cmbbxSex.Text,
+                Convert.ToInt32(txtbxPhoneNumber.Text),
+                txtbxEmailAddress.Text
+                );
+        } 
+        #endregion
+
+        #region [- BtnRefresh_Click -]
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            dgvPerson.DataSource = Ref_PersonViewModel.Refresh();
+        } 
+        #endregion
     }
 }
