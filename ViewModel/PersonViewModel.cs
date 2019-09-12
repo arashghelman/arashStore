eb35228e3cbf2f4d7a7f9981c0a3bf0af7465454
arash <arashghelman77@gmail.com>
2019-09-12 17:00:44 +0430

    Added Class PersonViewModel in ViewModel tier. Created 4 methods: add, remove, edit and refresh.

---
+++ ViewModel/PersonViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class PersonViewModel
+    {
+        #region [- ctor -]
+        public PersonViewModel()
+        {
+            Ref_PersonCrud = new Model.DomainModels.POCO.PersonCrud();
+        }
+        #endregion
+
+        #region [- props -]
+        public Model.DomainModels.POCO.PersonCrud Ref_PersonCrud { get; set; } 
+        #endregion
+
+        #region [- Add(string firstName, string lastName, int birthYear, string sex, int phoneNumber, string emailAddress) -]
+        public void Add(string firstName, string lastName, int birthYear, string sex, int phoneNumber, string emailAddress)
+        {
+            Ref_PersonCrud.Insert(firstName, lastName, birthYear, sex, phoneNumber, emailAddress);
+        }
+        #endregion
+
+        #region [- Remove() -]
+        public void Remove()
+        {
+            Ref_PersonCrud.Delete();
+        }
+        #endregion
+
+        #region [- Edit(int id, string firstName, string lastName, int birthYear, string sex, int phoneNumber, string emailAddress) -]
+        public void Edit(int id, string firstName, string lastName, int birthYear, string sex, int phoneNumber, string emailAddress)
+        {
+            Ref_PersonCrud.Update(id, firstName, lastName, birthYear, sex, phoneNumber, emailAddress);
+        }
+        #endregion
+
+        #region [- Refresh() -]
+        public dynamic Refresh()
+        {
+            return Ref_PersonCrud.Select();
+        } 
+        #endregion
+    }
+}
