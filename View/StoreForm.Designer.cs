b0ec778e9ff62a61ed7ff8ddb0705bb32a7230eb
arash <arashghelman77@gmail.com>
2019-09-17 21:31:05 +0430

    /

--- View/StoreForm.Designer.cs
+++ View/StoreForm.Designer.cs
@@ -28,7 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StoreForm));
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.btnManagement = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.AllowUserToAddRows = false;
+            this.dgvProducts.AllowUserToDeleteRows = false;
+            this.dgvProducts.AllowUserToResizeColumns = false;
+            this.dgvProducts.AllowUserToResizeRows = false;
+            this.dgvProducts.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(215)))), ((int)(((byte)(255)))));
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(204)))), ((int)(((byte)(255)))));
+            this.dgvProducts.Location = new System.Drawing.Point(62, 102);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.RowTemplate.Height = 60;
+            this.dgvProducts.Size = new System.Drawing.Size(435, 410);
+            this.dgvProducts.TabIndex = 0;
+            this.dgvProducts.Click += new System.EventHandler(this.DgvProducts_Click);
+            // 
+            // btnManagement
+            // 
+            this.btnManagement.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(213)))), ((int)(((byte)(255)))));
+            this.btnManagement.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManagement.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(15)))), ((int)(((byte)(236)))));
+            this.btnManagement.Location = new System.Drawing.Point(574, 33);
+            this.btnManagement.Name = "btnManagement";
+            this.btnManagement.Size = new System.Drawing.Size(165, 44);
+            this.btnManagement.TabIndex = 1;
+            this.btnManagement.Text = "Management";
+            this.btnManagement.UseVisualStyleBackColor = false;
+            this.btnManagement.Click += new System.EventHandler(this.BtnManagement_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("SketchFlow Print", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(46)))), ((int)(((byte)(243)))));
+            this.label1.Location = new System.Drawing.Point(53, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(456, 51);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome to Store";
             // 
             // StoreForm
             // 
@@ -37,12 +83,23 @@
             this.BackgroundImage = global::View.Properties.Resources.Untitled_Artwork_copy;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(764, 571);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnManagement);
+            this.Controls.Add(this.dgvProducts);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "StoreForm";
             this.Text = "StoreForm";
+            this.Load += new System.EventHandler(this.StoreForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.Button btnManagement;
+        private System.Windows.Forms.Label label1;
     }
 }
